38bc7d7c640876f5733188ca31734a7e233a59df
KEM-CONSOLATION <consolationlotachi@gmail.com>
2021-04-10 17:27:09 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("cart-info").addEventListener("click", function(){
+    var cart=document.getElementById("cart");
+    cart.classList.toggle("show-cart");
+    // console.log(cart);
+} );
